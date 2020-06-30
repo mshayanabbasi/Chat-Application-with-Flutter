34ce8ad0ca577e1ed5a22a057897adc45a3141dc
mshayanabbasi <mshayanabbasi98@gmail.com>
2020-06-30 14:47:43 +0500

    Updates

--- lib/main.dart
+++ lib/main.dart
@@ -13,15 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData.dark().copyWith(
-        textTheme: TextTheme(
-          body1: TextStyle(
-            color: Colors.black54,
-          ),
-        ),
-      ),
-      home: WelcomeScreen(),
+      debugShowCheckedModeBanner: false,
       initialRoute: WelcomeScreen.id,
       routes: {
         WelcomeScreen.id: (context) => WelcomeScreen(),
